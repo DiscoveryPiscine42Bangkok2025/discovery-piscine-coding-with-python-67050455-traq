dd9dc28db2b0226c7b7a4e4a705d6e346620dfdd
67050455-traq <67050455@kmitl.ac.th>
2025-09-17 15:24:05 +0700

    Add ex02: i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+while True:
+    user_input = input("What you gotta say? : ")
+    if user_input == "STOP":
+        break
+    print("I got that!")
+    user_input = input("Anything else? : ")
+    if user_input == "STOP":
+        break
+    print("I got that!")
