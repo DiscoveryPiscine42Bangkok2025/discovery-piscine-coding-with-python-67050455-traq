cdfed136a45b83dd33d2a733a8b5e6b758479ddc
67050455-traq <67050455@kmitl.ac.th>
2025-09-17 16:34:43 +0700

    Add ex13: append_it.py

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,11 @@
+import sys
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+    print("none")
+else:
+    for word in args:
+        if word.endswith("ism"):
+            continue
+        print(word + "ism")
